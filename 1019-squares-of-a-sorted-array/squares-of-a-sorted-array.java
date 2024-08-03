48820a609a71fbc40520c464b5855d1ca5e7d2f1
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2024-08-03 23:36:01 +0530

    Time: 19 ms (8.10%) | Memory: 46.3 MB (85.51%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,11 +1,7 @@
+import java.util.*;
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++)
-        {
-            int value=Math.abs(nums[i]);
-            nums[i]=value*value;
-        }
-        Arrays.sort(nums);
-        return nums;
-    }
+       List<Integer> li=Arrays.stream(nums).boxed().map(i->i*i).sorted().collect(Collectors.toList());
+       return li.stream().mapToInt(Integer::intValue).toArray();
+}
 }
