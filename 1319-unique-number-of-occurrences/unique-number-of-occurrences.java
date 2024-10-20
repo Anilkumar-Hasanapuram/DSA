39d9b7a4446ac38166f46daa849265eb765a6337
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2024-10-20 15:13:30 +0530

    Time: 2 ms (98.98%) | Memory: 42.3 MB (6.49%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -3,14 +3,7 @@ class Solution {
         Map<Integer,Integer> map=new HashMap<>();
         for(int value:arr)
         {
-            if(map.containsKey(value))
-            {
-                map.put(value,map.getOrDefault(value,0)+1);
-            }
-            else
-            {
-                map.put(value,1);
-            }
+            map.put(value,map.getOrDefault(value,0)+1);
         }
         Set<Integer> s=new HashSet<>(map.values());
 
