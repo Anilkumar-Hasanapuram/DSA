ddec8b806512a8948bf867c983c9f01395f5c445
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2024-11-05 22:26:30 +0530

    Time: 3 ms (97.78%) | Memory: 44.6 MB (73.44%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minChanges(String s) {
+        int changesNeeded = 0;
+        for (int i = 1; i < s.length(); i += 2) {
+            // If the current character is not the same as the previous character,
+            // we need to change it to make it the same
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                changesNeeded++; // Increment the counter for the number of changes needed
+            }
+        }
+      
+        // Return the total number of changes required to make the string's odd and even characters match
+        return changesNeeded;
+    }
+}
